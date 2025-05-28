1853eaba4953ade0e91a645ceee44524ac077b74
taro shinjyuku <tarosjk@gmail.com>
2025-05-28 13:38:12 +0900

    2-2for文完了

---
+++ lesson02/script.js
@@ -0,0 +1,11 @@
+// for文
+for (let i = 0; i < 5; i++) {
+  console.log(`iの値は${i}です`);
+}
+console.log('ループ終了');
+
+const maker = ['TOYOTA', 'HONDA', 'NISSAN', 'SUZUKI', 'TESLA'];
+console.log(maker);
+for (let i = 0; i < maker.length; i++) {
+  console.log(`車のメーカー: ${maker[i]}`);
+}
