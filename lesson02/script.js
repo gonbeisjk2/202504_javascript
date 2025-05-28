dfd66d95a613a23e7a99a3c8919b6becc8e8aeaf
taro shinjyuku <tarosjk@gmail.com>
2025-05-28 14:53:11 +0900

    論理演算の途中まで

--- lesson02/script.js
+++ lesson02/script.js
@@ -9,3 +9,44 @@ console.log(maker);
 for (let i = 0; i < maker.length; i++) {
   console.log(`車のメーカー: ${maker[i]}`);
 }
+
+let j = 0;
+while (j < 3) {
+  console.log(`jの値は${j}です`);
+
+  j++;
+}
+
+// if文
+const age = 18;
+if (age >= 18) {
+  console.log('あなたには選挙権があります');
+}
+
+const password = 'asdasdf';
+if (password.length >= 8) {
+  console.log('パスワードは8文字以上です');
+} else {
+  console.log('パスワードは8文字以上で設定してください');
+}
+
+if (password.length >= 12) {
+  console.log('とても強いパスワードです')
+} else if (password.length >= 8) {
+  console.log('良いパスワードです');
+} else {
+  console.log('パスワードは8文字以上で設定してください');
+}
+
+// 論理演算
+// password.length >= 12
+// password.includes('@') // true / false
+
+if (
+  password.length >= 12 ||
+  password.includes('@')
+) {
+  console.log('OK: とても強いパスワードです');
+} else {
+  console.log('NG: パスワードは12文字以上で@を含めてください');
+}
